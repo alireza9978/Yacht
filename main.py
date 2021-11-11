f011896eeb3d6e2fd9f7e5b8f546ca5aaab1c49a
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-11-11 18:10:48 +0330

    change prints

--- main.py
+++ main.py
@@ -7,18 +7,19 @@ def create_all_state():
         if dice_number >= Hand.dice_count:
             return [inner_hand.get_state()]
 
-        state_list = []
+        temp_state_list = []
         for inner_value in range(Dice.min_value, Dice.max_value + 1):
             inner_hand.set_dice_value(dice_number, inner_value)
-            state_list = state_list + inner_creator(inner_hand, dice_number + 1)
-        return state_list
+            temp_state_list = temp_state_list + inner_creator(inner_hand, dice_number + 1)
+        return temp_state_list
 
     hand = Hand()
     state_list = inner_creator(hand, 0)
-    print(len(state_list))
+    print("total state count = ", len(state_list))
     state_set = set(state_list)
-    print(len(state_set))
-    print(state_set)
+    print("unique state count = ", len(state_set))
+    for item in sorted(list(state_set)):
+        print(item)
 
 
 def test_hand():
